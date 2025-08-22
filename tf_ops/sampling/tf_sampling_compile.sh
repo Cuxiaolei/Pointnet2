2cd47cfa000292c93d83bd107bb54c67bd1baf30
cugitee <2289068115@qq.com>
2025-08-22 16:04:04 +0800

    11

--- tf_ops/sampling/tf_sampling_compile.sh
+++ tf_ops/sampling/tf_sampling_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 编译 CUDA 部分
 /usr/local/cuda/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
+#
 # 编译 C++ 部分（注意每行末尾的反斜杠和无空格）
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC \
 -I /root/miniconda3/lib/python3.8/site-packages/tensorflow/include \
